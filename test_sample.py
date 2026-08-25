c66acc8f58ceeaa33779d67d1ab561304d22dc7c
jpablodaza2007 <jpablodaza2007@gmail.com>
2026-08-25 12:34:22 -0500

    feat: agregada prueba unitaria status 200 y pipeline de dos jobs intenti 2

--- test_sample.py
+++ test_sample.py
@@ -1,11 +1,11 @@
 import pytest
-# IMPORTANTE: Reemplaza 'app' por el nombre de tu archivo de Flask (ej. de app import app)
-from app import app 
+# IMPORTANTE: Reemplaza 'sample_app' por el nombre de tu archivo de Flask (ej. de sample_app import sample_app)
+from sample_app import sample_app 
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
+    sample_app.config['TESTING'] = True
+    with sample_app.test_client() as client:
         yield client
 
 def test_home_status_200(client):
@@ -17,4 +17,3 @@ def test_home_status_200(client):
     assert response.status_code == 200
 
 
-    
