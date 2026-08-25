be3a37ff3d9778adb2867ac00dce82d97f3c8537
jpablodaza2007 <jpablodaza2007@gmail.com>
2026-08-25 12:39:24 -0500

    feat: agregada prueba unitaria status 200 y correccion importacion sample

--- test_sample.py
+++ test_sample.py
@@ -1,11 +1,10 @@
 import pytest
-# IMPORTANTE: Reemplaza 'sample_app' por el nombre de tu archivo de Flask (ej. de sample_app import sample_app)
-from sample_app import sample_app 
+from sample_app import sample
 
 @pytest.fixture
 def client():
-    sample_app.config['TESTING'] = True
-    with sample_app.test_client() as client:
+    sample.config['TESTING'] = True
+    with sample.test_client() as client:
         yield client
 
 def test_home_status_200(client):
